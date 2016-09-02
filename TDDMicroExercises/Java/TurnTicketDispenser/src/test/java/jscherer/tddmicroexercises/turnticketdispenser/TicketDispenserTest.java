f4037a57628b5620955cda4a182eafc20dca76a0
jscherer <josef.scherer@gmail.com>
2016-09-02 19:57:04 +0200

    java 1.8, copy source, TicketDispenser Test

---
+++ TDDMicroExercises/Java/TurnTicketDispenser/src/test/java/jscherer/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -0,0 +1,19 @@
+package jscherer.tddmicroexercises.turnticketdispenser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TicketDispenserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public final void testGetTurnTicket() {
+		fail("Not yet implemented");
+	}
+
+}
