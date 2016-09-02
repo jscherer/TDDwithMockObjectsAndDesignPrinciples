af7522281fc4965a07e8b93555f32b1cdb70976e
jscherer147 <josef.scherer@gmail.com>
2016-09-02 10:04:04 +0200

    Generated Tests

---
+++ TDDMicroExercises/Java/TextConverter/src/main/java/jscherer/textconvertor/UnicodeFileToHtmlTextConverterTest.java
@@ -0,0 +1,22 @@
+package jscherer.textconvertor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnicodeFileToHtmlTextConverterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testUnicodeFileToHtmlTextConverter() {
+		
+	}
+
+	@Test
+	public void testConvertToHtml() {
+		
+	}
+
+}
