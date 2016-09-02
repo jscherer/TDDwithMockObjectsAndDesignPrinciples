a710e400dc21943e73e6c7756dab16cdd6b9ae9a
jscherer147 <josef.scherer@gmail.com>
2016-09-02 08:32:35 +0200

    ISensor -> Sensor

---
+++ TDDMicroExercises/Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Sensor.java
@@ -0,0 +1,8 @@
+package tddmicroexercises.tirepressuremonitoringsystem;
+
+public interface Sensor {
+    final static double LOW_PRESSURE_THRESHOLD = 17;
+    final static double HIGH_PRESSURE_THRESHOLD = 21;
+
+	boolean isWithinThreshold();
+}
