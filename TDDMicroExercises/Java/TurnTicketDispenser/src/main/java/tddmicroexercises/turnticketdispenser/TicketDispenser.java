7e535aae5fb1f6bd54d3c3c990184c3cb683c989
jscherer <josef.scherer@gmail.com>
2016-09-03 08:44:07 +0200

    formatting

--- TDDMicroExercises/Java/TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenser.java
+++ TDDMicroExercises/Java/TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenser.java
@@ -1,13 +1,20 @@
 package tddmicroexercises.turnticketdispenser;
 
-public class TicketDispenser
-{
+/**
+ * Manages a queuing system in a shop.
+ * 
+ * There may be more than one ticket dispenser but the same ticket should not be
+ * issued to two different customers.
+ * 
+ * @author jscherer
+ *
+ */
+public class TicketDispenser {
 
-    public TurnTicket getTurnTicket()
-    {
-        int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
-        TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
+	public TurnTicket getTurnTicket() {
+		int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
+		TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
-        return newTurnTicket;
-    }
+		return newTurnTicket;
+	}
 }
