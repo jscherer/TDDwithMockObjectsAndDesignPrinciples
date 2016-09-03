224a299c15955558210853c90eec84e191c27785
jscherer <josef.scherer@gmail.com>
2016-09-03 16:38:14 +0200

    - Delegated next() to inner class DefaultSequence<TurnTicket> - added constructor to inject Sequence<TurnTicket>

--- TDDMicroExercises/Java/TurnTicketDispenser/src/main/java/jscherer/tddmicroexercises/turnticketdispenser/TicketDispenser.java
+++ TDDMicroExercises/Java/TurnTicketDispenser/src/main/java/jscherer/tddmicroexercises/turnticketdispenser/TicketDispenser.java
@@ -3,23 +3,47 @@ package jscherer.tddmicroexercises.turnticketdispenser;
 public class TicketDispenser implements Sequence<TurnTicket> {
 
 	protected static int turnNumber = 0;
-	
+	private Sequence<TurnTicket> sequence;
+
+	public TicketDispenser() {
+		this.sequence = new DefaultSequence();
+	}
+
+	/**
+	 * @param sequence
+	 */
+	public TicketDispenser(Sequence<TurnTicket> sequence) {
+		this.sequence = sequence;
+	}
+
+	/**
+	 * @return
+	 * @see jscherer.tddmicroexercises.turnticketdispenser.Sequence#next()
+	 */
+	public TurnTicket next() {
+		return sequence.next();
+	}
+
 	/**
 	 * deprecated use next() instead
+	 * 
 	 * @return TurnTicket with the next turnNumber
 	 */
 	public TurnTicket getTurnTicket() {
 		return next();
 	}
 
-	
-	/* (non-Javadoc)
-	 * @see jscherer.tddmicroexercises.turnticketdispenser.Sequence#next()
-	 * 
-	 * override to change behavior of TurnTicket Sequence (Sequence<TurnTicket>)
-	 */
-	@Override
-	public TurnTicket next() {
-		return new TurnTicket(turnNumber++);
+	class DefaultSequence implements Sequence<TurnTicket> {
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see jscherer.tddmicroexercises.turnticketdispenser.Sequence#next()
+		 */
+		@Override
+		public TurnTicket next() {
+			return new TurnTicket(turnNumber++);
+		}
+
 	}
 }
